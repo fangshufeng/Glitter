b5a419aedaaacb848717ad295eed7b783a76107b
fangshufeng <fangsh@dianrong.com>
2019-04-12 11:33:21 +0800

    [texture练习4]使用一个uniform变量作为mix函数的第三个参数来改变两个纹理可见度，使用上和下键来改变箱子或笑脸的可见度

--- Glitter/Sources/3.3.texture.fs
+++ Glitter/Sources/3.3.texture.fs
@@ -9,8 +9,9 @@ in vec2 ourTextCoords;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+uniform float mixValue;
 
 void main() {
 //    FragColor = mix(texture(texture1,ourTextCoords),texture(texture2,ourTextCoords),0.2)  ;
-     FragColor =  mix(texture(texture1, ourTextCoords), texture(texture2, vec2(1.0 - ourTextCoords.x, ourTextCoords.y)), 0.2);
+     FragColor =  mix(texture(texture1, ourTextCoords), texture(texture2, vec2(1.0 - ourTextCoords.x, ourTextCoords.y)), mixValue);
 }
--- Glitter/Sources/main_Texture.cpp
+++ Glitter/Sources/main_Texture.cpp
@@ -23,6 +23,9 @@ using namespace std;
 
 using namespace Our;
 
+float mixValue = 0.2f;
+void processInput(GLFWwindow *window);
+
 int main(int argc, char * argv[]) {
     
     
@@ -50,10 +53,10 @@ int main(int argc, char * argv[]) {
     
     float vertices[] = {
         // positions          // colors           // texture coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.55f, 0.55f, // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.55f, 0.45f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  0.45f, 0.45f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  0.45f, 0.55f  // top left
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
   
 
@@ -103,7 +106,7 @@ int main(int argc, char * argv[]) {
 
     // 设置纹理过滤
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
 
     // 读取图片到内存中
 
@@ -130,7 +133,7 @@ int main(int argc, char * argv[]) {
     
     // 设置纹理过滤
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     
     
     
@@ -153,13 +156,14 @@ int main(int argc, char * argv[]) {
     
     // Rendering Loop
     while (glfwWindowShouldClose(window) == false) {
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
+    
+        processInput(window);
         
         // Background Fill Color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glUniform1f(glGetUniformLocation(ourShader.programID, "mixValue"), mixValue);
         
         ourShader.user();
         
@@ -189,5 +193,23 @@ int main(int argc, char * argv[]) {
 
     
     return EXIT_SUCCESS;
+    
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        mixValue += 0.001f;
+        if(mixValue >= 1.0f)
+            mixValue = 1.0f;
+    }
+    
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        mixValue -= 0.001f;
+        if(mixValue <= 0.0f)
+            mixValue = 0.0f;
+    }
     
 }
