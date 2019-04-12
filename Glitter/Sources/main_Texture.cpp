d4f325fde394cfb183361200f08d7e3da48358ec
fangshufeng <fangsh@dianrong.com>
2019-04-12 10:02:25 +0800

    add texture demo

---
+++ Glitter/Sources/main_Texture.cpp
@@ -0,0 +1,155 @@
+
+// 抽取 编写、编译、管理着色器
+
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+#include <math.h>
+
+
+#include "Shader.hpp"
+#include "stb_image.h"
+
+using namespace std;
+
+using namespace Our;
+
+int main(int argc, char * argv[]) {
+    
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow *window =  glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+    if (window == nullptr) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    
+    
+    // ------------------------------------------------------------------
+    
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+  
+
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    
+    unsigned int vao,vbo,ebo;
+    glGenVertexArrays(1,&vao);
+    glGenBuffers(1,&vbo);
+    glGenBuffers(1,&ebo);
+    
+    glBindVertexArray(vao);
+    
+    glBindBuffer(GL_ARRAY_BUFFER,vbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2,2,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
+    
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    
+    glBindVertexArray(0);
+    
+    Shader  ourShader  = Shader("./3.3.texture.vs", "./3.3.texture.fs");
+    
+    
+    // 创建纹理
+    unsigned int texture;
+    glGenTextures(1,&texture);
+    glBindTexture(GL_TEXTURE_2D,texture); // 2d纹理
+
+    // 设置纹理的环绕方式
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+
+    // 设置纹理过滤
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+    // 读取图片到内存中
+
+    int width,height,nrChannels;
+    unsigned char *data = stbi_load("/Users/fangshufeng/Desktop/thirdPart/Glitter/container.jpg", &width, &height, &nrChannels, 0);
+
+    // d绑定到buffer上
+    if (data != nullptr) {
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    stbi_image_free(data);
+    
+    
+    // Rendering Loop
+    while (glfwWindowShouldClose(window) == false) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+        
+        // Background Fill Color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        
+        ourShader.user();
+        
+        glBindTexture(GL_TEXTURE_2D,texture);
+        glBindVertexArray(vao);
+        
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+//        glDrawArrays(GL_TRIANGLES,0,3);
+        
+        // Flip Buffers and Draw
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    
+    glfwTerminate();
+    
+
+    
+    return EXIT_SUCCESS;
+    
+}
