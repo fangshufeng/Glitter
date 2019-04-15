b2fb56d9c9cc1d347b49415a891a03871742e18b
fangshufeng <fangsh@dianrong.com>
2019-04-15 17:32:01 +0800

    [坐标系统]:2d图片3d显示

---
+++ Glitter/Sources/main_coordinate_systems.cpp
@@ -0,0 +1,237 @@
+
+// 抽取 编写、编译、管理着色器
+
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+#include <math.h>
+
+#include "Shader.hpp"
+#include "stb_image.h"
+
+using namespace std;
+
+using namespace Our;
+
+float mixValue = 0.2f;
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(int argc, char * argv[]) {
+    
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow *window =  glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello World", nullptr, nullptr);
+    if (window == nullptr) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    
+    
+    // ------------------------------------------------------------------
+    
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    
+    
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    
+    unsigned int vao,vbo,ebo;
+    glGenVertexArrays(1,&vao);
+    glGenBuffers(1,&vbo);
+    glGenBuffers(1,&ebo);
+    
+    glBindVertexArray(vao);
+    
+    glBindBuffer(GL_ARRAY_BUFFER,vbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)0);
+    glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    glVertexAttribPointer(2,2,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
+    
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    
+    glBindVertexArray(0);
+    
+    Shader  ourShader  = Shader("/Users/fangshufeng/Desktop/thirdPart/Glitter/Glitter/Sources/3.3.coordinate.vs", "/Users/fangshufeng/Desktop/thirdPart/Glitter/Glitter/Sources/3.3.coordinate.fs");
+    
+    
+    // 创建纹理
+    unsigned int texture;
+    glGenTextures(1,&texture);
+    glBindTexture(GL_TEXTURE_2D,texture); // 2d纹理
+    
+    // 设置纹理的环绕方式
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
+    
+    // 设置纹理过滤
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    
+    // 读取图片到内存中
+    
+    int width,height,nrChannels;
+    unsigned char *data = stbi_load("/Users/fangshufeng/Desktop/thirdPart/Glitter/container.jpg", &width, &height, &nrChannels, 0);
+    
+    // d绑定到buffer上
+    if (data != nullptr) {
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    
+    stbi_image_free(data);
+    
+    
+    // 创建纹理
+    unsigned int texture2;
+    glGenTextures(1,&texture2);
+    glBindTexture(GL_TEXTURE_2D,texture2); // 2d纹理
+    
+    // 设置纹理的环绕方式
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+    
+    // 设置纹理过滤
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    
+    
+    
+    int width2,height2,nrChannels2;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data2 = stbi_load("/Users/fangshufeng/Desktop/thirdPart/Glitter/awesomeface.png", &width2, &height2, &nrChannels2, 0);
+    
+    // 绑定到buffer上
+    if (data2 != nullptr) {
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width2,height2,0,GL_RGBA,GL_UNSIGNED_BYTE,data2);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    
+    stbi_image_free(data2);
+    
+    ourShader.user();
+    glUniform1i(glad_glGetUniformLocation(ourShader.programID,"texture1"),0);
+    glUniform1i(glad_glGetUniformLocation(ourShader.programID,"texture2"),1);
+
+    
+    // Rendering Loop
+    while (glfwWindowShouldClose(window) == false) {
+        
+        processInput(window);
+        
+        // Background Fill Color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUniform1f(glGetUniformLocation(ourShader.programID, "mixValue"), mixValue);
+        
+        ourShader.user();
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D,texture);
+        
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D,texture2);
+        
+        
+        // Model matrix
+        glm::mat4 model ;
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1,0,0));
+        
+        // view matrix
+        glm::mat4 view;
+        view = glm::translate(view, glm::vec3(0,0,-3.0f));
+        
+        // projection
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        
+        
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.programID,"model"),1, GL_FALSE,glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.programID,"view"),1, GL_FALSE,glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(ourShader.programID,"projection"),1, GL_FALSE,glm::value_ptr(projection));
+        
+        glBindVertexArray(vao);
+        
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        //        glDrawArrays(GL_TRIANGLES,0,3);
+        
+        // Flip Buffers and Draw
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    
+    glfwTerminate();
+    
+    
+    
+    return EXIT_SUCCESS;
+    
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        mixValue += 0.001f;
+        if(mixValue >= 1.0f)
+            mixValue = 1.0f;
+    }
+    
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        mixValue -= 0.001f;
+        if(mixValue <= 0.0f)
+            mixValue = 0.0f;
+    }
+    
+}
---
+++ main_debug.cpp
@@ -0,0 +1,141 @@
+
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+
+using namespace std;
+
+void APIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity,
+                            GLsizei length, const GLchar *message, void *userParam);
+
+GLenum glCheckError_(const char *file, int line)
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+
+int main(int argc, char * argv[]) {
+    
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLint flags;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        // 初始化调试输出
+        printf("dasdasdasas");
+    }
+    
+    GLFWwindow *window =  glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+    if (window == nullptr) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    
+    unsigned int textures;
+    glGenTextures(-5, &textures);
+    glCheckError();
+    
+    // Rendering Loop
+    while (glfwWindowShouldClose(window) == false) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+        
+        // Background Fill Color
+        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Flip Buffers and Draw
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    
+    return EXIT_SUCCESS;
+    
+}
+
+void APIENTRY glDebugOutput(GLenum source,
+                            GLenum type,
+                            GLuint id,
+                            GLenum severity,
+                            GLsizei length,
+                            const GLchar *message,
+                            void *userParam)
+{
+    // 忽略一些不重要的错误/警告代码
+    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+    
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+    
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+    
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+    
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
+}
