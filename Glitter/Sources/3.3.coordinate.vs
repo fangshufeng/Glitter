5818d56c269de5f685382b4de911c5d982ef883d
fangshufeng <fangsh@dianrong.com>
2019-04-17 14:33:38 +0800

    【坐标系统】立方体

--- Glitter/Sources/3.3.coordinate.vs
+++ Glitter/Sources/3.3.coordinate.vs
@@ -2,8 +2,10 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTextureCoords;
+layout (location = 1) in vec2 aTextureCoords;
+
+//layout (location = 1) in vec3 aColor;
+//layout (location = 2) in vec2 aTextureCoords;
 
 out vec4 ourColor;
 out vec2 ourTextCoords;
@@ -15,6 +17,6 @@ uniform mat4 projection;
 
 void main() {
     gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    ourColor = vec4(aColor,1.0f);
+//    ourColor = vec4(aColor,1.0f);
     ourTextCoords = aTextureCoords;
 }
--- Glitter/Sources/main_coordinate_systems.cpp
+++ Glitter/Sources/main_coordinate_systems.cpp
@@ -53,49 +53,110 @@ int main(int argc, char * argv[]) {
     glfwMakeContextCurrent(window);
     gladLoadGL();
     
+    glEnable(GL_DEPTH_TEST);
     
     // ------------------------------------------------------------------
     
+//    float vertices[] = {
+//        // positions          // colors           // texture coords
+//        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+//        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+//        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+//        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+//    };
+//
     float vertices[] = {
-        // positions          // colors           // texture coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
     
     
-    unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
+
+    
+//    unsigned int indices[] = {
+//        0, 1, 3, // first triangle
+//        1, 2, 3  // second triangle
+//    };
     
     unsigned int vao,vbo,ebo;
     glGenVertexArrays(1,&vao);
     glGenBuffers(1,&vbo);
-    glGenBuffers(1,&ebo);
+//    glGenBuffers(1,&ebo);
     
     glBindVertexArray(vao);
     
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_TRUE,5 * sizeof(float),(void *)0);
     glEnableVertexAttribArray(0);
     
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1,2,GL_FLOAT,GL_TRUE,5 * sizeof(float),(void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     
-    glVertexAttribPointer(2,2,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+//    glVertexAttribPointer(2,2,GL_FLOAT,GL_TRUE,8 * sizeof(float),(void *)(6 * sizeof(float)));
+//    glEnableVertexAttribArray(2);
     
     
     glBindBuffer(GL_ARRAY_BUFFER,0);
     
     glBindVertexArray(0);
+    
+    
     
     Shader  ourShader  = Shader("/Users/fangshufeng/Desktop/thirdPart/Glitter/Glitter/Sources/3.3.coordinate.vs", "/Users/fangshufeng/Desktop/thirdPart/Glitter/Glitter/Sources/3.3.coordinate.fs");
     
@@ -166,7 +227,8 @@ int main(int argc, char * argv[]) {
         
         // Background Fill Color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+//        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         glUniform1f(glGetUniformLocation(ourShader.programID, "mixValue"), mixValue);
         
@@ -178,15 +240,17 @@ int main(int argc, char * argv[]) {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D,texture2);
         
+        glBindVertexArray(vao);
         
         // Model matrix
         glm::mat4 model ;
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1,0,0));
-        
+//        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1,0,0));
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 0.5f, 0.0f));
+
         // view matrix
         glm::mat4 view;
         view = glm::translate(view, glm::vec3(0,0,-3.0f));
-        
+
         // projection
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -196,10 +260,10 @@ int main(int argc, char * argv[]) {
         glUniformMatrix4fv(glGetUniformLocation(ourShader.programID,"view"),1, GL_FALSE,glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(ourShader.programID,"projection"),1, GL_FALSE,glm::value_ptr(projection));
         
-        glBindVertexArray(vao);
         
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
-        //        glDrawArrays(GL_TRIANGLES,0,3);
+        
+//        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        glDrawArrays(GL_TRIANGLES,0,36);
         
         // Flip Buffers and Draw
         glfwSwapBuffers(window);
