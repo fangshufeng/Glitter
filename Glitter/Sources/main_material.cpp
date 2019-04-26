6b1028a28462339d6edc4edfea64d11a75e91be9
fangshufeng <fangsh@dianrong.com>
2019-04-26 16:01:11 +0800

    修改光源的颜色

--- Glitter/Sources/main_material.cpp
+++ Glitter/Sources/main_material.cpp
@@ -186,22 +186,22 @@ int main(int argc, char * argv[]) {
         glUniform1f(glGetUniformLocation(ourShader.programID, "material.shininess"), 32.0f);
         
         
-//        glm::vec3 lightColor;
-//        lightColor.x = sin(glfwGetTime() * 2.0f);
-//        lightColor.y = sin(glfwGetTime() * 0.7f);
-//        lightColor.z = sin(glfwGetTime() * 1.3f);
-//
-//        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // 降低影响
-//        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // 很低的影响
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+
+        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // 降低影响
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // 很低的影响
 
         
-        glUniform3f(glGetUniformLocation(ourShader.programID, "light.ambient"),  0.1f,0.1f,0.1f  );
-        glUniform3f(glGetUniformLocation(ourShader.programID, "light.diffuse"),  1.0f, 1.0f, 1.0f);
-        glUniform3f(glGetUniformLocation(ourShader.programID, "light.specular"),  .5f, .5f, 0.5f);
+//        glUniform3f(glGetUniformLocation(ourShader.programID, "light.ambient"),  0.1f,0.1f,0.1f  );
+//        glUniform3f(glGetUniformLocation(ourShader.programID, "light.diffuse"),  1.0f, 1.0f, 1.0f);
+//        glUniform3f(glGetUniformLocation(ourShader.programID, "light.specular"),  .5f, .5f, 0.5f);
         
-//        glUniform3fv(glGetUniformLocation(ourShader.programID, "light.ambient"),  1,&ambientColor[0] );
-//        glUniform3fv(glGetUniformLocation(ourShader.programID, "light.diffuse"),  1,&diffuseColor[0]);
-//        glUniform3f(glGetUniformLocation(ourShader.programID, "light.specular"),  1.0f, 1.0f, 1.0f);
+        glUniform3fv(glGetUniformLocation(ourShader.programID, "light.ambient"),  1,&ambientColor[0] );
+        glUniform3fv(glGetUniformLocation(ourShader.programID, "light.diffuse"),  1,&diffuseColor[0]);
+        glUniform3f(glGetUniformLocation(ourShader.programID, "light.specular"),  1.0f, 1.0f, 1.0f);
         
         
         //        glUniform3fv(glGetUniformLocation(ourShader.programID, "lightPos"), 1, &lightPos[0]);
