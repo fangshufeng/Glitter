94b2218c7a88b5a66010477ac284467f04950a4f
fangshufeng <fangsh@dianrong.com>
2019-04-28 15:20:22 +0800

    点光源

--- Glitter/Sources/3.3.light_caster.fs
+++ Glitter/Sources/3.3.light_caster.fs
@@ -11,11 +11,17 @@ struct Material {
 uniform Material material;
 
 struct Light {
-//    vec3 position;
+    vec3 position;
+    
     vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+    
+    float constant;
+    float linear;
+    float quadratic;
+    
 };
 
 uniform Light light;
@@ -35,9 +41,10 @@ void main()
     // 环境光
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, ourTextCoords));
     
+    
     // 漫反射
     vec3 norm = normalize(Normal);
-    vec3 lightDir =  normalize(-light.direction); //normalize(lightPos - FragPos);
+    vec3 lightDir =  normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, ourTextCoords));
     
@@ -46,6 +53,15 @@ void main()
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec *  vec3(texture( material.specular,ourTextCoords));
+    
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance +
+                               light.quadratic * (distance * distance));
+    
+    
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
     
     vec3 result = ambient + diffuse + specular ;
     
--- Glitter/Sources/main_light_caster.cpp
+++ Glitter/Sources/main_light_caster.cpp
@@ -209,18 +209,21 @@ int main(int argc, char * argv[]) {
         
         glUniform3f(glGetUniformLocation(ourShader.programID, "lightColor"),  1.0f, 1.0f, 1.0f);
         
-//        glUniform3f(glGetUniformLocation(ourShader.programID, "lightPos"),  1.2f, 1.0f, 2.0f);
+        glUniform3f(glGetUniformLocation(ourShader.programID, "lightPos"),  1.2f, 1.0f, 2.0f);
         
         glUniform3fv(glGetUniformLocation(ourShader.programID, "viewPos"),  1,&camera.Position[0]);
         
         
         glUniform1f(glGetUniformLocation(ourShader.programID, "material.shininess"), 32.0f);
         
-        glUniform3f(glGetUniformLocation(ourShader.programID, "light.direction"),  -1.2f, -1.0f, -2.0f);
+//        glUniform3f(glGetUniformLocation(ourShader.programID, "light.direction"),  -1.2f, -1.0f, -2.0f);
         glUniform3f(glGetUniformLocation(ourShader.programID, "light.ambient"),  0.1f,0.1f,0.1f  );
         glUniform3f(glGetUniformLocation(ourShader.programID, "light.diffuse"),  1.0f, 1.0f, 1.0f);
         glUniform3f(glGetUniformLocation(ourShader.programID, "light.specular"),  .5f, .5f, 0.5f);
         
+        glUniform1f(glGetUniformLocation(ourShader.programID, "light.constant"), 1.0f);
+        glUniform1f(glGetUniformLocation(ourShader.programID, "light.linear"), 0.09f);
+        glUniform1f(glGetUniformLocation(ourShader.programID, "light.quadratic"), 0.032f);
         
         // Model matrix
         glm::mat4 model = glm::mat4(1.0f);
