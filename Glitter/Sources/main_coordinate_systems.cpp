9069f59fd923a59496d3d7aec64d09e15fc97419
fangshufeng <fangsh@dianrong.com>
2019-05-05 13:51:00 +0800

    模型加载

--- Glitter/Sources/main_coordinate_systems.cpp
+++ Glitter/Sources/main_coordinate_systems.cpp
@@ -62,7 +62,6 @@ int main(int argc, char * argv[]) {
     gladLoadGL();
     
     glEnable(GL_DEPTH_TEST);
-    
     // ------------------------------------------------------------------
     
 //    float vertices[] = {
